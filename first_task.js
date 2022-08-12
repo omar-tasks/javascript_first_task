b36727634ad96e6abfbe5b0fb5ecc9f799efc6ec
oamrkhalil97 <omar.khalil9780@gmail.com>
2022-08-12 15:18:52 +0300

    javascript_task1

---
+++ first_task.js
@@ -0,0 +1,23 @@
+var x = 25;
+var y = 50;
+var z = 100;
+
+var sum = x + y + z;
+var proposal = z - y - x;
+var Multipli = x * y * z;
+var division = z / y / x;
+var avg = sum / 3;
+
+// concatination ex 
+
+var name = 'omar';
+var real = 'khawal';
+var result = name + real;
+
+console.log(sum);
+console.log(proposal);
+console.log(Multipli);
+console.log(division);
+console.log(avg);
+console.log(result);
+console.log('with om ayman love')
